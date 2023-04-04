b0f9f3a101cf10b63025b8f0bccaeb0c9fc95233
Givenchie <moretigiven@gmail.com>
2023-04-04 20:47:53 +0200

    Extra checking

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # SU5_1
 nickname = input("Enter your nickname: ")
 hobby = input("Enter your hobby: ")
+# use formatted strings to print nickname and hobby
 print(f" your nickname is {nickname} and your hobby is {hobby}!")
-
+# \n to print the results on a new line
 character = input("Enter a character: \n Steps")
 # predefined rows and columns
 rows = 6
@@ -15,9 +16,9 @@ for i in range(rows):
 # SU_2
 # allow the user to enter their age from the terminal and save it in a variable age
 age = input("Enter your age: ")
-# allow the user's friend to enter their age from the terminaland save it in a variable friend age
+# allow the user's friend to enter their age from the terminal and save it in a variable friend_age
 friend_age = input("Enter the age of your friend: ")
-# concatenate the ages and print in terminal
+# concatenate the ages and print them in terminal
 print("Result: " + (age + friend_age))
 
 
@@ -29,5 +30,7 @@ age = int(input("Enter your age: "))
 friend_age = int(input("Enter the age of your friend: "))
 result = age + friend_age
 print("Result: " + str(result))
+# get the average of the ages
 average_age = int(result)/2
+# typecast the ages and print in terminal 
 print("Average age: " + str(average_age))
