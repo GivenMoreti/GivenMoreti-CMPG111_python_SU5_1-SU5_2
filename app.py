2cca890576e0c698f3af347e2c768e4ae4a855b3
Givenchie <moretigiven@gmail.com>
2023-04-04 20:42:33 +0200

    make corrections

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 # SU5_1
-# nickname = input("Enter your nickname: ")
-# hobby = input("Enter your hobby: ")
-# print(f" your nickname is {nickname} and your hobby is {hobby}!")
+nickname = input("Enter your nickname: ")
+hobby = input("Enter your hobby: ")
+print(f" your nickname is {nickname} and your hobby is {hobby}!")
 
-# character = input("Enter a character: \n Steps")
-# # predefined rows and columns
-# rows = 6
-# for i in range(rows):
-#   for j in range(i):
-#     print(character,end="")
-# # creates a blank line between each column
-#   print()
-
-
+character = input("Enter a character: \n Steps")
+# predefined rows and columns
+rows = 6
+for i in range(rows):
+  for j in range(i):
+    print(character,end="")
+# creates a blank line between each column
+  print()
 
 # SU_2
 # allow the user to enter their age from the terminal and save it in a variable age
@@ -28,7 +26,6 @@ print("Sum values")
 # allow the user to enter their age from the terminal and save it in a variable age and typecast it as int
 age = int(input("Enter your age: "))
 # allow the user's  friend to enter their age from the terminal and save it in a variable friend_age and typecast it as int
-age = input("Enter your age: ")
 friend_age = int(input("Enter the age of your friend: "))
 result = age + friend_age
 print("Result: " + str(result))
